c4fb90f5adf157cb980ed51ba9a64e6e64d72d0c
Wes Akers <wes.akers@39alpha.io>
2023-12-16 02:13:28 -0800

    Basic endpoints and migrations Add a few gets and posts for the resources Add the initial migration for the db... oops!

--- src/Henry.Api/Data/Appointment.cs
+++ src/Henry.Api/Data/Appointment.cs
@@ -1,10 +1,10 @@
 ﻿namespace Henry.Api.Data
 {
-    public class Appointments
+    public class Appointment
     {
         public int Id { get; set; }
-        public Provider Provider { get; set; }
-        public Client Client { get; set; }
+        public required Provider Provider { get; set; }
+        public required Client Client { get; set; }
         public DateOnly AppointmentOn { get; set; }
         public TimeOnly AppointmentFrom { get; set; }
         public TimeOnly AppointmentTo { get; set; }
--- src/Henry.Api/Program.cs
+++ src/Henry.Api/Program.cs
@@ -11,6 +11,53 @@ using var scope = app.Services.CreateScope();
 var db = scope.ServiceProvider.GetService<HenryDbContenxt>();
 db?.Database.MigrateAsync();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/appointments", async (HenryDbContenxt db) => await db.Appointments.ToListAsync());
+
+app.MapGet("/providers", async (HenryDbContenxt db) => await db.Providers.ToListAsync());
+
+app.MapGet("/clients", async (HenryDbContenxt db) => await db.Clients.ToListAsync());
+
+app.MapGet("/providers/{id}/availability", async (int Id, HenryDbContenxt db) =>
+{
+    var provider = await db.Providers.FindAsync(Id);
+    if (provider is null)
+        return Results.NotFound();
+
+    var providerAvailability = await db.ProviderAvailabilities.Where(x => x.Provider.Id == Id).ToListAsync();
+    return Results.Ok(providerAvailability);
+});
+
+app.MapPost("/appointments", async (HenryDbContenxt db, Appointment appointment) =>
+{
+    await db.Appointments.AddAsync(appointment);
+    await db.SaveChangesAsync();
+    return Results.Created($"/appointments/{appointment.Id}", appointment);
+});
+
+app.MapPost("/providers", async (HenryDbContenxt db, Provider provider) =>
+{
+    await db.Providers.AddAsync(provider);
+    await db.SaveChangesAsync();
+    return Results.Created($"/providers/{provider.Id}", provider);
+});
+
+app.MapPost("/clients", async (HenryDbContenxt db, Client client) =>
+{
+    await db.Clients.AddAsync(client);
+    await db.SaveChangesAsync();
+    return Results.Created($"/clients/{client.Id}", client);
+});
+
+app.MapPost("/providers/{id}/availability", async (int Id, HenryDbContenxt db, ProviderAvailability providerAvailability) =>
+{
+    var provider = await db.Providers.FindAsync(Id);
+    if (provider is null)
+        return Results.NotFound();
+
+    providerAvailability.Provider = provider;
+    await db.ProviderAvailabilities.AddAsync(providerAvailability);
+    await db.SaveChangesAsync();
+    return Results.Created($"/providers/{provider.Id}/availability/{providerAvailability.Id}", providerAvailability);
+});
 
 app.Run();
