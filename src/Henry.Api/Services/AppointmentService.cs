8c16d321a03d34e972a2086400127b0895f225b4
Wes Akers <wes.akers@39alpha.io>
2023-12-16 19:21:20 -0800

    Configure kestrel for dev Configures the kestrel web server in dev to use the same port Updates the http file to use the configured dev port

---
+++ src/Henry.Api/Services/AppointmentService.cs
@@ -0,0 +1,29 @@
+﻿using Henry.Api.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace Henry.Api.Services
+{
+    public sealed class AppointmentService
+    {
+        public AppointmentService()
+        {
+            
+        }
+
+        public Appointment? AddAppointment(Appointment appointment)
+        {
+            if (!AppointmentIs15MinuteBlock(appointment))
+                throw new ValidationException("Appointments must be in 15 minute blocks");
+            return default;
+        }
+
+        // Probably a custom validatation attribute would be a better approach here but this will do for time constraints
+        private bool AppointmentIs15MinuteBlock(Appointment appointment)
+        {
+            var duration = appointment.AppointmentTo - appointment.AppointmentFrom;
+            if (duration != TimeSpan.FromMinutes(15))
+                return false;
+            return true;
+        }
+    }
+}
